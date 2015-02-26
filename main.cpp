8189344bba268232652e1f83c3bb8a3b3d18c80d
amcmaho4 <amcmaho4@nd.edu>
2015-02-26 15:17:31 -0500

    this prints out the binary matix of valid locations

--- main.cpp
+++ main.cpp
@@ -11,15 +11,29 @@ using namespace std;
 int main(){
 
 	
-string fileName;
-cout << "Please enter file name: ";
-cin >> fileName;
-
-sodoku <char> puzzle("wordoku.txt");
-	puzzle.print();
-//	//puzzle.play();
+//string fileName;
+//cout << "Please enter file name: ";
+//cin >> fileName;
 	
-	sodoku <int> puzzle2("sodoku.txt");
-	puzzle2.print();
 	
+//int userChoice;
+//// lets the user play the two versions of the game
+//cout<< "would you like to play sodoku(1) or wordoku(2)? "<< endl;
+//	cin>> userChoice;
+	
+	//if(userChoice==1){  // playing actual sodoku
+		sodoku <int> puzzle("easySodoku.txt");
+		puzzle.print();
+	puzzle.updateUntakenValues();
+		//puzzle.insertSingle();
+		//puzzle.solve();
+	//}
+//	else if(userChoice==2){ // palying wordoku
+//		sodoku <char> puzzle("wordoku.txt");
+//		puzzle.print();
+//		puzzle.play();
+//	}
+//	else{
+//		cout<< "that wasnt an option"<< endl;
+//	}
 }
--- sodoku.h
+++ sodoku.h
@@ -15,12 +15,12 @@ using namespace std;
 
 #include <vector>
 #include <algorithm>
-#include <Array>
 #include <iterator>
 #include <fstream>
 #include <ctype.h>
 #include <typeinfo>
 #include <map>
+#include<numeric>
 
 template <typename T>
 class sodoku  {
@@ -28,70 +28,46 @@ class sodoku  {
 	
 public:
 	sodoku(string);
-	//~sodoku();
 	void print();
 	void play();
+	bool Won();
 	bool checkIfInvalid(T, int , int );
 	T *squareValuesPtr[9][9];
 	int squareVal[9][9];
 	T *verticalValuesPtr[9][9];
 	T *horizontalValuesPtr[9][9];
+	vector<T> getValidArray(int , int );
+	void updateUntakenValues();
+	void insertSingle();
 	
+	//T*[9][9] getsquareValuesPtr()
 private:
-	
+	vector<vector <vector <T> > > untakenValues;
 	vector<vector <T> > puzzle;
 	T *ptr; // pointer to first element of pointer-based array	
 };
 
 
 
-// Data members
 template<typename T>
 sodoku<T>::sodoku(string fileName){
+	// input the file
 	ifstream inFile;
 	inFile.open (fileName.c_str());
-	int sizeOfBoard = 9;
+	
+	
+	//read the contents of te file into a vector of vectors (the puzzle) using temporary vars
 	T tempVar;
-	 string line;
-	int index =0;
-//	while ( getline (inFile,line) ){
-//		puzzle.push_back( vector<T> ());
-//		for(int i = 0 ; i < line.size() ; i++){    //Read in a line into a temporary vector
-//			if(!isspace(line[i])){			//check to see if it is not a space
-//				if (isdigit(line[i])){
-//						puzzle[index].push_back(line[i]-'0');
-//						if (puzzle[index][puzzle[index].size()-1] == char(NULL))
-//							//cout << "I'm null..." << endl;
-//							puzzle[index][puzzle[index].size()-1] = '0';
-//					
-//				}
-//				else
-//						puzzle[index].push_back(line[i]);
-//						
-//			}
-//			
-//			// do diiferent things depending on whether it is a char or int typeid
-//		}
-//		index++;
-//	}
-	
-	
-
 	vector<T> tempVec;
-
 	while(!inFile.eof()){ //While the file is not at the end
-		for(int i = 0 ; i < sizeOfBoard ; i++){    //Read in a line into a temporary vector
+		for(int i = 0 ; i < 9 ; i++){    //Read in a line into a temporary vector
 			inFile >> tempVar; //Put just read value into temporary vector
 			tempVec.push_back(tempVar);
-			//cout<< tempVar;
 		}
 		puzzle.push_back(tempVec);   //Push back the line just read)
 		tempVec.clear(); //Clear/recycle temporary vector
 	}
-//
 
-	
-//
 	// make an array of pointers to the loactions columns
 	for (int v=0; v<9; v++) {
 		for(int l=0; l< 9 ; l++){
@@ -104,6 +80,8 @@ sodoku<T>::sodoku(string fileName){
 			horizontalValuesPtr[v][l]= &(puzzle[l][v]);
 		}
 	}
+	
+	// squareCenters holds the locations of all the centers of the 3x3 sub- squares that make up a 9 x9 grid
 	int squareCenters[9][2] = {
 		{1,1},
 		{1,4},
@@ -116,6 +94,7 @@ sodoku<T>::sodoku(string fileName){
 		{7,7}
 	};
 	
+	// locations is essentially an array of increments, that when added to squared centers, will give you all the locations in a mini grid. For example, if you add locations to squareCenters[1] .. (1,1) then you would get a 9x2 matrix. This 9x2 matrix holds all the locations of the upper left mini grid in the full grid
 	int locations[9][2] =
 		{{1, -1},
 		{-1, 1},
@@ -139,90 +118,247 @@ sodoku<T>::sodoku(string fileName){
 			squareValuesPtr[c][l]= &(puzzle[i][j]);
 			squareVal[i][j]=c;  // used to decide which square the piece is in
 		}
-	}
-
-	
-	
-	
+}
 }
 //
+
 template<typename T>
 void sodoku<T>::print(){
-	for(int i= 0; i< puzzle.size(); i++){
-		for(int j=0; j< puzzle.size() ; j++){
-			cout<< puzzle[i][j];
+	cout<< endl;
+	for(int i= 0; i< 9; i++){
+		for(int j=0; j< 9 ; j++){
+			cout<< puzzle[i][j]<< " ";
+		}
+		cout<< endl;
+
+	}
+
+}
+
+
+template<typename T>
+void sodoku<T>::play(){
+	T value;
+	int X=0;
+	int Y=0;
+	bool gameLost = false;  //if they enter in a bad location
+	bool gameWon= false;
+	while ( !(gameLost)) {
+		bool badUserInput=true;
+		gameWon= Won();
+		if (gameWon)
+			continue;
+		while(badUserInput==true){ //while loop continues to read user locations until they enter a valid one
+			cout<< "enter in the X and Y position: (rows/ columns go from 0-8)"<< endl;
+
+			cout<< "X position: " <<endl;
+			cin >> X;
+			cout << "Y position: "<< endl;
+			cin>> Y;
+	
+			if(X<0 |Y<0 |X>9 |Y>9) // check the user input
+				cout<< "that spot is out side of the bounds";
+			else if(puzzle[X][Y]!=0 && puzzle[X][Y]!='0')
+				cout<< "that spot is taken";
+			else
+				badUserInput=false;
+			cout<< endl;
+		}
+			cout << "and what's you value: "<<endl;
+		cin>> value;
+		
+		
+		
+		if (checkIfInvalid(value, X,Y)) // check the invalid array
+		{
+			gameLost= true;
+			cout<<endl;
+			cout << "sorry bad move you lose";
+			cout<<endl;
+
+		}
+		else{
+			puzzle[X][Y]=value;
+			cout<< "successfully placed, updated puzzle: "<<endl;
+			print();
+			gameWon= Won();
+			cout<<endl;
+			
+		}
+	}
+}
+
+
+
+// checks to see if all the spaces on the board are filled
+template<typename T>
+bool sodoku<T>::Won(){
+	bool winner= true;
+		for(int i= 0; i< 9; i++){
+			for(int j=0; j< 9 ; j++){
+				if(puzzle[i][j]==0 || puzzle[i][j]=='0'){
+					winner= false;
+				}
+			}
+		}
+	if(winner ==true){
+		cout << "winner"<< endl;
+		exit(1);
+	}
+	
+	return winner;
+}
+
+
+// check if valid gets all the elements and pushes
+
+template<typename T>
+bool sodoku<T>::checkIfInvalid(T value, int X, int Y){
+	// use push back to put all the relevant values into a vector
+	// the vector has 0's and is 27 T in size
+	vector<T> invalid;
+	for(int l=0; l< 9 ; l++){
+		invalid.push_back(*(squareValuesPtr[squareVal[X][Y]][l]));
+		invalid.push_back(*(verticalValuesPtr[l][Y]));
+		invalid.push_back(*(horizontalValuesPtr[l][X]));
+	}
+	// fix it up into a nice vector with only one copy of each invalid element
+	
+	//erase all of the 0's
+	invalid.erase(std::remove(invalid.begin(), invalid.end(), 0), invalid.end());
+	// erase all the '0''s
+	invalid.erase(std::remove(invalid.begin(), invalid.end(), '0'), invalid.end());
+
+	
+	std::sort(invalid.begin(), invalid.end());
+	invalid.erase(std::unique(invalid.begin(), invalid.end()), invalid.end());
+	cout<<endl;
+//	
+//	cout<< "the invalid numbers were: "<<endl;
+//	for(int j=0; j< invalid.size() ; j++){
+//		cout<<invalid[j];
+//	}
+
+	//returns true or false based on whether the users input was good or not
+	return (std::find(invalid.begin(), invalid.end(), value) != invalid.end());
+	
+}
+
+
+
+template<typename T>
+vector<T> sodoku<T>::getValidArray(int X, int Y){
+vector<T> invalid;
+for(int l=0; l< 9 ; l++){
+	invalid.push_back(*(squareValuesPtr[squareVal[X][Y]][l]));
+	invalid.push_back(*(verticalValuesPtr[l][Y]));
+	invalid.push_back(*(horizontalValuesPtr[l][X]));
+}
+// fix it up into a nice vector with only one copy of each invalid element
+
+//erase all of the 0's
+invalid.erase(std::remove(invalid.begin(), invalid.end(), 0), invalid.end());
+// erase all the '0''s
+invalid.erase(std::remove(invalid.begin(), invalid.end(), '0'), invalid.end());
+
+
+std::sort(invalid.begin(), invalid.end());
+invalid.erase(std::unique(invalid.begin(), invalid.end()), invalid.end());
+cout<<endl;
+
+vector<T> untakenValues;
+for(int i=1; i<=9; i++){
+	if((std::find(invalid.begin(), invalid.end(), i) != invalid.end())){
+		untakenValues.push_back(0);
+	}
+	else{
+		untakenValues.push_back(1);
+	}
+}
+	return untakenValues;
+}
+
+
+
+
+template<typename T>
+void sodoku<T>::updateUntakenValues(){
+	vector<T> untakenValuesSegment;
+	vector< vector<T> > untakenValues2Segment;
+	cout<< endl;
+	cout<< endl;
+	for(int i= 0; i< 9; i++){
+		for(int j=0; j< 9 ; j++){
+			untakenValuesSegment= getValidArray(i,j);
+			//untakenValuesSegment.erase(std::remove(untakenValuesSegment.begin(), untakenValuesSegment.end(), 0), untakenValuesSegment.end());
+			for(int j=0; j< untakenValuesSegment.size() ; j++){
+				cout<<untakenValuesSegment[j];
+			}
+			untakenValues2Segment.push_back(untakenValuesSegment);
+			untakenValuesSegment.clear();
+		}
+		cout<< endl;
+		untakenValues.push_back(untakenValues2Segment);
+		
+		untakenValues2Segment.clear();
+	}
+	for(int i= 0; i< 9; i++){
+		for(int j=0; j< 9 ; j++){
+			for (int k=0; k<9; k++) {
+				cout<< untakenValues[i][j][k];
+		}
+			cout<< " ";
+		}
+		cout<< endl;
+	}
+	
+	
+	
+
+	
+}
+
+
+template<typename T>
+void sodoku<T>:: insertSingle(){
+	updateUntakenValues();
+	int yesContinue=0;
+	while(!(Won())){
+	for(int i= 0; i< 9; i++){
+		for(int j=0; j< 9 ; j++){
+			if(puzzle[i][j]==0){
+			if(((untakenValues[i][j]).size()) <= 4){
+				puzzle[i][j]=1; //untakenValues[i][j][1];
+				updateUntakenValues();
+				i=0;
+				j=0;
+				cout<< "this is the board: "<<endl;
+				print();
+				cout << "continue?"<< endl;
+				cin>> yesContinue;
+			}
+			}
+			print();
+	}
+	}
+
+}
+}
+
+			
+
+template<typename T>
+void printVectorVectorVector(){
+	cout<< endl;
+	for(int i= 0; i< 9; i++){
+		for(int j=0; j< 9 ; j++){
+			for (int k=0; k<9; k++) {
+				//cout<< puzzle[i][j][k]<< " ";
+			}
+			cout<<endl;
 		}
 		cout<< endl;
 	}
 }
-//
-//
-//template<typename T>
-//void sodoku<T>::play(){
-//	T value;
-//	int X=0;
-//	int Y=0;
-//	int elementsPlaced = 0;
-//	int emptylocations = 81; //CHANGE
-//	bool gameOver = false;  //if they enter in a bad location
-//	while ((elementsPlaced <= emptylocations) && !(gameOver)) {
-//		
-//		do{
-//			cout<< "X position: " <<endl;
-//			cin >> X;
-//			cout << endl;
-//			cout << "Y position: "<< endl;
-//			cin>> Y;
-//		}while(puzzle[X][Y]!=0);
-//		cout << "and what's you value: "<<endl;
-//		cin>> value;
-//		
-//		
-//		
-//		if (checkIfInvalid(value, X,Y)) // check the invalid array
-//		{
-//			gameOver= true;
-//			cout<<endl;
-//			cout << "sorry bad move you lose";
-//		}
-//		else{
-//			puzzle[X][Y]=value;
-//			elementsPlaced++;
-//			cout<< "successfully placed, updated puzzle: "<<endl;
-//			print();
-//			cout<<endl;
-//		}
-//	}
-//}
-//
-//
-//
-//// check if valid gets all the elements and pushes
-//
-//template<typename T>
-//bool sodoku<T>::checkIfInvalid(T value, int X, int Y){
-//// use push back to put all the relevant values into a vector
-//// the vector has 0's and is 27 T in size
-//vector<T> invalid;
-//for(int l=0; l< 9 ; l++){
-//	invalid.push_back(*(squareValuesPtr[squareVal[X][Y]][l]));
-//	invalid.push_back(*(verticalValuesPtr[l][Y]));
-//	invalid.push_back(*(horizontalValuesPtr[l][X]));
-//}
-//// fix it up into a nice vector with only one copy of each invalid element
-//invalid.erase(std::remove(invalid.begin(), invalid.end(), 0), invalid.end());
-////
-//std::sort(invalid.begin(), invalid.end());
-//invalid.erase(std::unique(invalid.begin(), invalid.end()), invalid.end());
-//cout<<endl;
-//for(int j=0; j< invalid.size() ; j++){
-//	cout<< "the invalid numbers were: "<<invalid[j]<<endl;
-//}
-//
-////returns true or false based on whether the users input was good or not
-//return (std::find(invalid.begin(), invalid.end(), value) != invalid.end());
-//	
-//}
-
 
 #endif /* defined(____sodoku__) */
